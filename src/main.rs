b7729bd7ca2fc11a6a0e3e42235ef50c738c35f5
Julius de Jeu <julius@voidcorp.nl>
2022-10-30 17:59:09 +0100

    Add simple nand gate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod component;
+use std::collections::HashMap;
+
+use component::*;
+use uuid::Uuid;
+
 fn main() {
-    println!("Hello, world!");
+    let mut nand = ComponentInstance::new(Component::Nand);
+    let mut wires = HashMap::new();
+    let wire1 = Uuid::new_v4();
+    let wire2 = Uuid::new_v4();
+    let output = nand.add_output();
+    nand.add_input(wire1);
+    nand.add_input(wire2);
+    wires.insert(wire1, SignalState::High);
+    wires.insert(wire2, SignalState::High);
+    nand.exec(&mut wires, 0).unwrap();
+    println!("{:?}", wires.get(&output));
+
 }
